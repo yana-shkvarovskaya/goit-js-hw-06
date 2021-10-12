b272d0802c70746801f64edfa9befb850202f7ee
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-10-12 13:45:21 +0300

    1 задание

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const allItems = document.querySelectorAll('.item');
+console.log('Number of categories:', `${allItems.length}`);
+
+const list = document.querySelectorAll('#categories>li');
+list.forEach(item => {
+
+  console.log(
+`Category: ${item.firstElementChild.textContent}
+Elements: ${item.lastElementChild.children.length}`,
+  );
+});
