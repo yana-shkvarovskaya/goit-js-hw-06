8f210f69c2644c960dd4bf477bd22ebcc0f5367d
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-10-13 20:12:12 +0300

    добавляет задание 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+
+    const email = event.currentTarget.email;
+    const password = event.currentTarget.password;
+
+    if (email.value === '' || password.value === ''){
+        alert('All the fields of the form are obligatory');
+    } 
+
+    const formData = {
+            email: email.value,
+            password: password.value,
+        }
+
+        console.log(formData);
+       
+        event.currentTarget.reset();
+}       
